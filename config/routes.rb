440d3f22748492bbc6f8e38622cab2b374243291
Keith Gan <keithgan96@gmail.com>
2018-12-02 00:39:10 +0800

    Created sign in feature with google

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
   
   get  'welcome/index'
   root 'welcome#index'
-
+  
   resources :users
   resources :groups
-
+  
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
 end
