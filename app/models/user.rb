fc3a635d416a85bf4c69a8f901f002e1b6151138
Keith Gan <keithgan96@gmail.com>
2018-12-01 17:41:05 +0800

    Added validations for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     has_secure_password
+    has_many :groups, dependent: :destroy
+
+    validates :email, format: { with: VALID_EMAIL_REGEX}
 end
