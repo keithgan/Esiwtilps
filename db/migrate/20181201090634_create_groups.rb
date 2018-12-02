17da7b340245a1e4edbfc0a1b1d0f3a62c2825b5
Keith Gan <keithgan96@gmail.com>
2018-12-02 16:15:34 +0800

    Linked user and group with foreign key

--- db/migrate/20181201090634_create_groups.rb
+++ db/migrate/20181201090634_create_groups.rb
@@ -2,7 +2,8 @@ class CreateGroups < ActiveRecord::Migration[5.2]
   def change
     create_table :groups do |t|
       t.string :name, null: false
-
+      t.references :users, foreign_key: true
+      
       t.timestamps
     end
   end
