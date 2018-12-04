c5f5d86cda6fe2fa012694db62557ae677560faa
Keith Gan <keithgan96@gmail.com>
2018-12-04 23:37:30 +0800

    Created membership model to link user and group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
-    has_and_belongs_to_many :user, dependent: :destroy
+    has_many :memberships
+    has_many :user, dependent: :destroy
     include PgSearch
     pg_search_scope :omnisearch, :against => :name
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     has_secure_password
     has_many :authentications, dependent: :destroy
+    has_many :memberships
     has_many :groups, dependent: :destroy
     has_many :friends, dependent: :destroy
     
