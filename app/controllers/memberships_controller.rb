bb9a3bd66560b1697fe9baa80099ee77ccdb2471
Keith Gan <keithgan96@gmail.com>
2018-12-05 01:29:00 +0800

    Created my groups page and successfully linked user to group

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,18 +1,18 @@
-class FriendsController < ApplicationController
-    def befriend 
+class MembershipsController < 
+    def join 
         @user = User.find(params[:id])
+        @group = Group.fine(params[:id])
 
-        friend = Friend.new(user_id: current_user.id, friends_with: @user.id)
+        membership = Membership.new(user_id: current_user.id, group_id: @group.id)
         friend.save
         redirect_to "/users/#{@user.id}"
     end
 
-    def unfriend
+    def leave
         @user = User.find(params[:id])
 
         friend_to_delete = Friend.find_by(user_id: current_user.id, friends_with: @user.id)
         friend_to_delete.destroy
         redirect_to "/users/#{@user.id}"
     end
-    
 end
