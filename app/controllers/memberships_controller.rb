0c5759618108582467fda487cc8c265425c6edcb
Keith Gan <keithgan96@gmail.com>
2018-12-05 17:33:12 +0800

    Can successfully join and leave from index page

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,7 +1,6 @@
-class MembershipsController < 
+class MembershipsController < ApplicationController
     def join 
-        @user = User.find(params[:id])
-        @group = Group.fine(params[:id])
+        @group = Group.find(params[:id])
 
         membership = Membership.new(user_id: current_user.id, group_id: @group.id)
         membership.save
@@ -9,10 +8,10 @@ class MembershipsController <
     end
 
     def leave
-        @user = User.find(params[:id])
+        @group = Group.find(params[:id])
 
-        friend_to_delete = Friend.find_by(user_id: current_user.id, friends_with: @user.id)
-        friend_to_delete.destroy
+        membership_to_delete = Membership.find_by(user_id: current_user.id, group_id: @group.id)
+        membership_to_delete.destroy
         redirect_to groups_path
     end
 end
